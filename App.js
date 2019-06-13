1484420cdb2067c36c19b7f0becffa4755b367df
GodfreyR555 <51419769+GodfreyR555@users.noreply.github.com>
2019-06-13 08:08:39 -0400

    App.js
    This is the react/jsx file.

---
+++ App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import airplane from './airplane.jpg';
+import './App.css';
+
+function Home() {
+  return (
+    <div className="Home">
+	
+	<div className="Home-prag">
+	<a href= "http://www.pragmatics.com/">Pragmatics</a>
+	</div>
+      <header className="Home-header">
+        
+        <p>
+          Pragmatics Air
+        </p>
+      
+      </header>
+	<nav className = "Home-nav">
+	  <pre>
+ 	   Log in    Sign Up    Language    Search
+	  </pre>
+	</nav>
+
+	<body className="Home-body">
+	  <aside className ="Home-dir">
+	    <p> Home</p><p>
+View Listings </p><p>
+Hotels</p><p>
+Dining</p><p>
+Shopping</p><p>
+Parking</p><p>
+Log in</p><p>
+Help </p>
+	  </aside>
+
+
+	  <section className="Home-image">
+	
+	    <img src={airplane} className="Home-pic" alt="pic"/>
+
+	  </section>
+
+	</body>
+	<footer className="Home-footer">
+	  &copy;2019 Pragmatics Inc. All Rights Reserved.
+	</footer>
+
+    </div>
+  );
+}
+
+export default Home;
